53f2cf1bef1372ac151a6842b9aacd052c37fcc7
Dogeerproxy <168145051+Dogeerproxy@users.noreply.github.com>
2024-04-30 22:36:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const express = require("express")
-const https = require("https")
-const app = express()
-
-app.get("/chess/*", (req, res, next) => {
-  const pathSegments = req.path.split('/');
-  const discordPath = pathSegments[2]
-  https.request(new URL("https://chess.com/" + discordPath), (resp) => {
-    res.contentType(resp.headers["content-type"])
-    resp.pipe(res)
-  }).end()
-})
-app.get("/*", (req, res, next) => {
-  //const pathSegments = req.path.split('/');
- // const rocketPath = pathSegments[2]
-  https.request(new URL("https://"http://rocketbotroyale2.winterpixel.io" + req.path), (resp) => {
-    res.contentType(resp.headers["content-type"])
-    resp.pipe(res)
-  }).end()
-})
 
 
-app.listen(3000)
+
+
+
+
+  
+  
+
+  
+
+    
+    
+    
+
+    
+
+  
+
+  
+
+                    
+
+
+
+
+
+
+    
